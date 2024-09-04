7f41b4e344c489df7f8920852c1227b5e1bf6663
usein <useinakbar01@gmail.com>
2024-09-04 13:49:28 +0700

    fix: loader

--- src/components/atoms/Loader.jsx
+++ src/components/atoms/Loader.jsx
@@ -3,7 +3,7 @@ import loader from '../../assets/loader.png'
 
 const Loader = () => {
   return (
-    <div className="grid place-items-center fixed inset-0 z-20 bg-secondary">
+    <div className="grid place-items-center fixed inset-0 z-20 bg-background">
       <img src={loader} alt="Rick and Morty" className="w-24 md:w-48" />
     </div>
   )
